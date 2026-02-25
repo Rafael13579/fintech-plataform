2594750167e1332557278aee37990428ececcd29
Rafael13579 <rafaelfernandesdasilva567@gmail.com>
2026-02-24 22:19:31 -0300

    feat: First commit

---
+++ account-service/account-service/src/main/java/com/fintech/account/model/Account.java
@@ -0,0 +1,31 @@
+package com.fintech.account.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "accounts")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String document;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @Column(nullable = false)
+    private String holderName;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
