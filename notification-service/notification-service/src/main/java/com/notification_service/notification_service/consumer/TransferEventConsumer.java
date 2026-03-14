71edee0a28e9bd9588a2d12625e605247926a43a
Rafael13579 <rafaelfernandesdasilva567@gmail.com>
2026-03-14 18:59:53 -0300

    feat: add dockerfile

--- notification-service/notification-service/src/main/java/com/notification_service/notification_service/consumer/TransferEventConsumer.java
+++ notification-service/notification-service/src/main/java/com/notification_service/notification_service/consumer/TransferEventConsumer.java
@@ -3,12 +3,25 @@ package com.notification_service.notification_service.consumer;
 import com.notification_service.notification_service.event.TransferCompletedEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class TransferEventConsumer {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @KafkaListener(topics = "transfer.completed", groupId = "notification-service-group")
-    public void listen(TransferCompletedEvent event) {
-        System.out.println("Transfer completed of id: " + event.transactionId());
+    public void listen(String message) {
+        try {
+            TransferCompletedEvent event = objectMapper.readValue(message, TransferCompletedEvent.class);
+
+            System.out.println("Transfer completed");
+            System.out.println("Transaction: " + event.transactionId());
+            System.out.println("From: " + event.fromAccountId());
+            System.out.println("To: " + event.toAccountID());
+            System.out.println("Amount: " + event.amount());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
